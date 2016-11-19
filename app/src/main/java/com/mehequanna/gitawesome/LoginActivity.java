1ceeffac3141a4edc1d8ec2341a2862a56778571
Stephen Emery <emery.stephen@gmail.com>
2016-11-18 16:29:45 -0800

    Add bindings and clicklistener in LoginActivity.java.

--- app/src/main/java/com/mehequanna/gitawesome/LoginActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/LoginActivity.java
@@ -2,12 +2,35 @@ package com.mehequanna.gitawesome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.signInButton) Button mSignInButton;
+    @Bind(R.id.loginEditText) EditText mLogInEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.hiddenTextView) TextView mHiddenTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mSignInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSignInButton) {
+            mHiddenTextView.setVisibility(v.VISIBLE);
+            Toast.makeText(LoginActivity.this, "Signed In!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
