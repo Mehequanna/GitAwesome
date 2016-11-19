0a4f103797fbf1f75078c74be188db27bb621fd8
Stephen Emery <emery.stephen@gmail.com>
2016-11-18 21:00:22 -0800

    Add List to print to list view on UserActivity.

--- app/src/main/java/com/mehequanna/gitawesome/UserActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/UserActivity.java
@@ -3,6 +3,9 @@ package com.mehequanna.gitawesome;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -10,6 +13,11 @@ import butterknife.ButterKnife;
 
 public class UserActivity extends AppCompatActivity {
     @Bind(R.id.userTextView) TextView mUserTextView;
+    @Bind(R.id.languagesTextView) TextView mLanguagesTextView;
+    @Bind(R.id.profileImageView) ImageView mProfileImageView;
+    @Bind(R.id.languageListView) ListView mLanguagesListView;
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    private String[] languagesList = new String[] {"This list", "Will hold", "The users", "Saved", "Languages"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,8 @@ public class UserActivity extends AppCompatActivity {
         String username = "Current User: \n" + intent.getStringExtra("username");
 
         mUserTextView.setText(username);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, languagesList);
+        mLanguagesListView.setAdapter(adapter);
     }
 }
