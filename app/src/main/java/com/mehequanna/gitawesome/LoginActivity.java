0e389d2d0d6d6932df554138f0fca9bd17f2b2fa
Stephen Emery <emery.stephen@gmail.com>
2016-11-18 17:26:26 -0800

    Add log in form validation.

--- app/src/main/java/com/mehequanna/gitawesome/LoginActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/LoginActivity.java
@@ -2,6 +2,8 @@ package com.mehequanna.gitawesome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.loginEditText) EditText mLogInEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.hiddenTextView) TextView mHiddenTextView;
+    public static final String TAG = "log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +32,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         if (v == mSignInButton) {
-            mHiddenTextView.setVisibility(v.VISIBLE);
-            Toast.makeText(LoginActivity.this, "Signed In!", Toast.LENGTH_SHORT).show();
+            if (TextUtils.isEmpty(mPasswordEditText.getText().toString().trim()) && TextUtils.isEmpty(mLogInEditText.getText().toString().trim())) {
+                Toast.makeText(LoginActivity.this, "Please enter a username and password", Toast.LENGTH_SHORT).show();
+                mHiddenTextView.setVisibility(v.VISIBLE);
+            } else if (TextUtils.isEmpty(mPasswordEditText.getText().toString().trim())) {
+                Toast.makeText(LoginActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+                mHiddenTextView.setVisibility(v.VISIBLE);
+            } else if (TextUtils.isEmpty(mLogInEditText.getText().toString().trim())) {
+                Toast.makeText(LoginActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+                mHiddenTextView.setVisibility(v.VISIBLE);
+            } else if (mPasswordEditText.getText().toString().trim().equals("password") && mLogInEditText.getText().toString().trim().equals("user")) {
+                Toast.makeText(LoginActivity.this, "Signed In!", Toast.LENGTH_SHORT).show();
+                mHiddenTextView.setVisibility(v.VISIBLE);
+            } else if (!(mPasswordEditText.getText().toString().trim().equals("password") && mLogInEditText.getText().toString().trim().equals("user"))) {
+                Toast.makeText(LoginActivity.this, "Incorrect Login", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
