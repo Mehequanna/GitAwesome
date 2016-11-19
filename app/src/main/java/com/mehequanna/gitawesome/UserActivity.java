f25aed52575c3f448289bdad4c07af36f474d5b1
Stephen Emery <emery.stephen@gmail.com>
2016-11-18 19:51:41 -0800

    Use intent to add username to UserActivity.

--- app/src/main/java/com/mehequanna/gitawesome/UserActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/UserActivity.java
@@ -1,13 +1,25 @@
 package com.mehequanna.gitawesome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class UserActivity extends AppCompatActivity {
+    @Bind(R.id.userTextView) TextView mUserTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String username = "Current User: \n" + intent.getStringExtra("username");
+
+        mUserTextView.setText(username);
     }
 }
