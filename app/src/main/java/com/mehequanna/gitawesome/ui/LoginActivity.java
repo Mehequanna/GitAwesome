4174be944cc7d980bef80a57388a9c19f4acc5c8
Stephen Emery <emery.stephen@gmail.com>
2016-12-02 10:39:01 -0800

    Add ui folder and move activities.

--- app/src/main/java/com/mehequanna/gitawesome/ui/LoginActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/LoginActivity.java
@@ -1,15 +1,16 @@
-package com.mehequanna.gitawesome;
+package com.mehequanna.gitawesome.ui;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mehequanna.gitawesome.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
