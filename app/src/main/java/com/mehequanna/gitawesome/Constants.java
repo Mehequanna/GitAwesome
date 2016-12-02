fae0dfdda3c9978f448f7eb6380a07b3ceba4482
Stephen Emery <emery.stephen@gmail.com>
2016-12-02 10:47:12 -0800

    Add constants class.

---
+++ app/src/main/java/com/mehequanna/gitawesome/Constants.java
@@ -0,0 +1,10 @@
+package com.mehequanna.gitawesome;
+
+/**
+ * Created by stephenemery on 12/2/16.
+ */
+
+public class Constants {
+    public static final String GIT_CLIENT_ID = BuildConfig.GIT_CLIENT_ID;
+    public static final String GIT_CLIENT_SECRET = BuildConfig.GIT_CLIENT_SECRET;
+}
