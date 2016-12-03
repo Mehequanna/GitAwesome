b602cd871b3b0364bdde25e3b086cb61418d0f9d
Stephen Emery <emery.stephen@gmail.com>
2016-12-02 17:19:37 -0800

    Add intent to pass info from landing to gits activity.

--- app/src/main/java/com/mehequanna/gitawesome/ui/LandingActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/LandingActivity.java
@@ -3,6 +3,7 @@ package com.mehequanna.gitawesome.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,7 +44,17 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
         } else if (v == mSearchMeetupButton) {
             Toast.makeText(LandingActivity.this, "Meetup Search Function Coming Soon!", Toast.LENGTH_LONG).show();
         } else if (v == mSearchGitButton) {
-            Toast.makeText(LandingActivity.this, "Git Search Function Coming Soon!", Toast.LENGTH_LONG).show();
+            if (TextUtils.isEmpty(mLanguageEditText.getText().toString().trim())) {
+                Toast.makeText(LandingActivity.this, "Please enter a language.", Toast.LENGTH_LONG).show();
+            } else if (!(TextUtils.isEmpty(mLanguageEditText.getText().toString().trim()))) {
+                if (!(TextUtils.isEmpty(mZipEditText.getText().toString().trim()))) {
+                    Toast.makeText(LandingActivity.this, "No zip needed for git searches.", Toast.LENGTH_SHORT).show();
+                }
+                String language = mLanguageEditText.getText().toString().trim();
+                Intent intent = new Intent(LandingActivity.this, GitsActivity.class);
+                intent.putExtra("language", language);
+                startActivity(intent);
+            }
         } else if (v == mAboutButton) {
             Toast.makeText(LandingActivity.this, "About Page Coming Soon!", Toast.LENGTH_LONG).show();
         }
