6164ddddc4dba7b413088f57be52bbcf1474488a
Stephen Emery <emery.stephen@gmail.com>
2016-12-02 18:23:30 -0800

    Add getRepos method.

--- app/src/main/java/com/mehequanna/gitawesome/ui/GitsActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/GitsActivity.java
@@ -3,12 +3,19 @@ package com.mehequanna.gitawesome.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.mehequanna.gitawesome.R;
+import com.mehequanna.gitawesome.services.GitService;
+
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class GitsActivity extends AppCompatActivity {
     @Bind(R.id.textView2) TextView mTextView;
@@ -20,8 +27,31 @@ public class GitsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        String language = "Language: " + intent.getStringExtra("language");
+        String language = intent.getStringExtra("language");
 
+        getRepos(language);
         mTextView.setText(language);
     }
+
+    private void getRepos(String language) {
+        final GitService gitService = new GitService();
+        gitService.findRepos(language, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v("log", jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
