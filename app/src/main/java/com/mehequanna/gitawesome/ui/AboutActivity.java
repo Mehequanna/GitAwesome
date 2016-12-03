235494bb3b3bd633e02882a99e3062dc18e50a87
Stephen Emery <emery.stephen@gmail.com>
2016-12-02 23:32:45 -0800

    Updated About activity with icon, link, and text.

--- app/src/main/java/com/mehequanna/gitawesome/ui/AboutActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/AboutActivity.java
@@ -1,15 +1,35 @@
 package com.mehequanna.gitawesome.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mehequanna.gitawesome.R;
 
-public class AboutActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.websiteTextView) TextView mWebsiteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+
+        mWebsiteTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteTextView) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/Mehequanna/GitAwesome"));
+            startActivity(webIntent);
+        }
     }
 }
