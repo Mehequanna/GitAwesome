1855ee5802a33b0f3cbebdc863c1048d5fb6c962
Stephen Emery <emery.stephen@gmail.com>
2016-12-02 16:54:08 -0800

    Update landing activity layout and click listeners.

--- app/src/main/java/com/mehequanna/gitawesome/ui/LandingActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/LandingActivity.java
@@ -15,7 +15,9 @@ import butterknife.ButterKnife;
 
 public class LandingActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.loginButton) Button mLoginButton;
-    @Bind(R.id.searchButton) Button mSearchButton;
+    @Bind(R.id.searchGitButton) Button mSearchGitButton;
+    @Bind(R.id.searchMeetupButton) Button mSearchMeetupButton;
+    @Bind(R.id.aboutButton) Button mAboutButton;
     @Bind(R.id.languageEditText) EditText mLanguageEditText;
     @Bind(R.id.zipEditText) EditText mZipEditText;
     public static final String TAG = "log";
@@ -28,7 +30,9 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
         ButterKnife.bind(this);
 
         mLoginButton.setOnClickListener(this);
-        mSearchButton.setOnClickListener(this);
+        mSearchMeetupButton.setOnClickListener(this);
+        mSearchGitButton.setOnClickListener(this);
+        mAboutButton.setOnClickListener(this);
     }
 
     @Override
@@ -36,8 +40,12 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
         if (v == mLoginButton) {
             Intent intent = new Intent(LandingActivity.this, LoginActivity.class);
             startActivity(intent);
-        } else if (v == mSearchButton) {
-            Toast.makeText(LandingActivity.this, "Search Function Coming Soon!", Toast.LENGTH_LONG).show();
+        } else if (v == mSearchMeetupButton) {
+            Toast.makeText(LandingActivity.this, "Meetup Search Function Coming Soon!", Toast.LENGTH_LONG).show();
+        } else if (v == mSearchGitButton) {
+            Toast.makeText(LandingActivity.this, "Git Search Function Coming Soon!", Toast.LENGTH_LONG).show();
+        } else if (v == mAboutButton) {
+            Toast.makeText(LandingActivity.this, "About Page Coming Soon!", Toast.LENGTH_LONG).show();
         }
     }
 }
