0c2554e0ec409a0dec61fe64b73097b7a3ed0dbf
Stephen Emery <emery.stephen@gmail.com>
2016-12-09 13:01:47 -0800

    Add createNewUser method.

--- app/src/main/java/com/mehequanna/gitawesome/ui/CreateAccountActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/CreateAccountActivity.java
@@ -1,19 +1,28 @@
 package com.mehequanna.gitawesome.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mehequanna.gitawesome.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
     @Bind(R.id.createUserButton) Button mCreateUserButton;
     @Bind(R.id.loginTextView) TextView mLoginTextView;
     @Bind(R.id.confirmPasswordEditText) EditText mConfirmPassword;
@@ -23,6 +32,8 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.usernameEditText) EditText mUsernameEditText;
     @Bind(R.id.nameEditText) EditText mNameEditText;
 
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +41,10 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mLoginTextView.setOnClickListener(this);
-//        mCreateUserButton.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
     }
 
     @Override
@@ -44,7 +57,26 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         }
 
         if (view == mCreateUserButton) {
-//            createNewUser();
+            createNewUser();
         }
+    }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confrimPassword = mConfirmPassword.getText().toString().trim();
+        
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication Successful");
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
