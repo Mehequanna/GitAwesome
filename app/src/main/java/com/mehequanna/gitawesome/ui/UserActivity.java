531fa6fc6bdc180330b9b05776562fe52f629e4e
Stephen Emery <emery.stephen@gmail.com>
2016-12-09 14:36:26 -0800

    Add menu for logout in user activity.

--- app/src/main/java/com/mehequanna/gitawesome/ui/UserActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/UserActivity.java
@@ -3,6 +3,9 @@ package com.mehequanna.gitawesome.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,6 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mehequanna.gitawesome.R;
 
 import butterknife.Bind;
@@ -46,5 +50,30 @@ public class UserActivity extends AppCompatActivity {
                 Toast.makeText(UserActivity.this, language, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_user, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(UserActivity.this, LandingActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
