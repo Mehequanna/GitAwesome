655b0574da2871f3e11b4101aa8abc20a081e6cc
Stephen Emery <emery.stephen@gmail.com>
2016-12-09 15:25:38 -0800

    Add loginFormValidation method.

--- app/src/main/java/com/mehequanna/gitawesome/ui/LoginActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/LoginActivity.java
@@ -3,13 +3,12 @@ package com.mehequanna.gitawesome.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mehequanna.gitawesome.R;
 
 import butterknife.Bind;
@@ -21,13 +20,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.hiddenTextView) TextView mHiddenTextView;
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
-    public static final String TAG = LoginActivity.class.getSimpleName();;
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mSignInButton.setOnClickListener(this);
         mRegisterTextView.setOnClickListener(this);
@@ -40,25 +43,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intent);
             finish();
         }
-
-
-        // This code checks that the user has filled all fields and used the correct login credentials.
         if (v == mSignInButton) {
-            if (TextUtils.isEmpty(mPasswordEditText.getText().toString().trim()) && TextUtils.isEmpty(mLogInEditText.getText().toString().trim())) {
-                Toast.makeText(LoginActivity.this, "Please enter a username and password", Toast.LENGTH_SHORT).show();
-            } else if (TextUtils.isEmpty(mPasswordEditText.getText().toString().trim())) {
-                Toast.makeText(LoginActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
-            } else if (TextUtils.isEmpty(mLogInEditText.getText().toString().trim())) {
-                Toast.makeText(LoginActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
-            } else if (mPasswordEditText.getText().toString().trim().equals("password") && mLogInEditText.getText().toString().trim().equals("user")) {
-                String username = mLogInEditText.getText().toString().trim();
-                Intent intent = new Intent(LoginActivity.this, UserActivity.class);
-                intent.putExtra("username", username);
-                startActivity(intent);
-            } else if (!(mPasswordEditText.getText().toString().trim().equals("password") && mLogInEditText.getText().toString().trim().equals("user"))) {
-                Toast.makeText(LoginActivity.this, "Incorrect Login", Toast.LENGTH_SHORT).show();
-            }
-                mHiddenTextView.setVisibility(v.VISIBLE);
+            loginFormValidation();
+        }
+    }
+
+    private void loginFormValidation() {
+        String email = mLogInEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mLogInEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
         }
     }
 }
