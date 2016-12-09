e6e2a965f08c44d9e4084e9d991466118257975f
Stephen Emery <emery.stephen@gmail.com>
2016-12-09 12:06:31 -0800

    Add binds and click listeners to CreateAccountActivity.

--- app/src/main/java/com/mehequanna/gitawesome/ui/CreateAccountActivity.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/CreateAccountActivity.java
@@ -1,15 +1,50 @@
 package com.mehequanna.gitawesome.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mehequanna.gitawesome.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPassword;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.zipEditText) EditText mZipEditText;
+    @Bind(R.id.usernameEditText) EditText mUsernameEditText;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+//        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mCreateUserButton) {
+//            createNewUser();
+        }
     }
 }
