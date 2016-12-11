34b1613365c177a1aa5d7e4c2f742f73ca716370
Stephen Emery <emery.stephen@gmail.com>
2016-12-10 16:59:00 -0800

    Update Gits Detail Fragment to link Repo class to view.

--- app/src/main/java/com/mehequanna/gitawesome/ui/GitsDetailFragment.java
+++ app/src/main/java/com/mehequanna/gitawesome/ui/GitsDetailFragment.java
@@ -6,25 +6,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mehequanna.gitawesome.R;
+import com.mehequanna.gitawesome.models.Repo;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class GitsDetailFragment extends Fragment {
+    @Bind(R.id.gitsImageView) ImageView mGitsImageView;
+    @Bind(R.id.gitsNameTextView) TextView mGitsNameTextView;
+    @Bind(R.id.descriptionTextView) TextView mDescriptionTextView;
+    @Bind(R.id.createdTextView) TextView mCreatedTextView;
+    @Bind(R.id.updatedTextView) TextView mUpdatedTextView;
+    @Bind(R.id.stargazersTextView) TextView mStargazersTextView;
+    @Bind(R.id.githubTextView) TextView mGithubTextView;
+    @Bind(R.id.saveGitButton) Button mSaveGitButton;
 
+    private Repo mRepo;
 
-    public GitsDetailFragment() {
-        // Required empty public constructor
+    public static GitsDetailFragment newInstance(Repo repo) {
+        GitsDetailFragment gitsDetailFragment = new GitsDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("repo", Parcels.wrap(repo));
+        gitsDetailFragment.setArguments(args);
+        return gitsDetailFragment;
     }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gits_detail, container, false);
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mRepo = Parcels.unwrap(getArguments().getParcelable("repo"));
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gits_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mRepo.getAvatar()).into(mGitsImageView);
+
+        mGitsNameTextView.setText(mRepo.getName());
+        mDescriptionTextView.setText(mRepo.getDescription());
+        mCreatedTextView.setText(mRepo.getCreatedAt());
+        mUpdatedTextView.setText(mRepo.getUpdatedAt());
+        mStargazersTextView.setText(mRepo.getStargazers());
+        mGithubTextView.setText(mRepo.getWebsite());
+
+        return view;
+    }
 }
