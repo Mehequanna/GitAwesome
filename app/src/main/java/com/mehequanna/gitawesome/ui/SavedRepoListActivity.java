e8930b60371ae4c162d0e1c44df163d7cce1ed11
Stephen Emery <emery.stephen@gmail.com>
2016-12-10 20:46:05 -0800

    Add Saved Repo List Activity.

---
+++ app/src/main/java/com/mehequanna/gitawesome/ui/SavedRepoListActivity.java
@@ -0,0 +1,12 @@
+package com.mehequanna.gitawesome.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedRepoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
